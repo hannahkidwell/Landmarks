7cf4cfbc23a674d034e0f414c1db3128996dc238
Hannah Kidwell <kidwellhe@gmail.com>
2020-01-02 18:55:28 -1000

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Hannah Kidwell on 1/2/20.
+//  Copyright © 2020 Hannah Kidwell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
